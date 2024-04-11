98d5b6812518272ab743bf6fb5d33b1326fd5519
suking <akindpepe@gmail.com>
2024-04-12 06:13:29 +0800

    M

--- abpay/get_switch_game_lists.php
+++ abpay/get_switch_game_lists.php
@@ -5,7 +5,7 @@ require_once 'databaseConnection.php';
 // 假设您已经连接到数据库
 
 $connection = new DatabaseConnection();
-$pdo = $connection->connect();
+$pdo = $connection->connect('utf8mb4');
 
 // 准备和执行查询
 $stmt = $pdo->prepare('SELECT * FROM switch_game_lists ');
@@ -13,5 +13,5 @@ $stmt->execute();
 $game_lists = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 // 將遊戲名稱以 JSON 格式回傳
-header('Content-Type: application/json');
+header('Content-Type: application/json; charset=UTF-8');
 echo json_encode($game_lists);
