22c531e60e3b6f7b349b7398894938dc35552e6d
suking <akindpepe@gmail.com>
2024-04-12 06:54:20 +0800

    M

--- get_switch_game_lists.php
+++ get_switch_game_lists.php
@@ -2,16 +2,16 @@
 
 // 連接資料庫
 require_once 'databaseConnection.php';
+// 假设您已经连接到数据库
 
 $connection = new DatabaseConnection();
 $pdo = $connection->connect();
 
-$sql = "SELECT Sid,Id,Name,flag FROM switch_game_lists  ";
-$stmt = $pdo->prepare($sql);
+// 准备和执行查询
+$stmt = $pdo->prepare('SELECT * FROM switch_game_lists ');
 $stmt->execute();
-$data = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
+$game_lists = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 // 將遊戲名稱以 JSON 格式回傳
 header('Content-Type: application/json');
-echo json_encode($data);
+echo json_encode($game_lists);
