73d43a7ae395ebf86d6bb3e8e69cb6de2de4acf4
SuHsienChin <akindpepe@gmail.com>
2025-03-15 18:28:58 +0800

    M

--- abpay/index.php
+++ abpay/index.php
@@ -8,8 +8,6 @@ require_once 'databaseConnection.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@400;500;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/admin.css">
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
     <link rel="stylesheet" href="https://cdn.datatables.net/2.0.3/css/dataTables.dataTables.min.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
@@ -18,30 +16,24 @@ require_once 'databaseConnection.php';
 </head>
 
 <body>
+
     <div class="container-fluid">
         <div class="row">
             <!-- Sidebar -->
-            <nav id="sidebar">
+            <nav id="sidebar" class="col-md-3 col-lg-2 d-md-block bg-light sidebar">
                 <div class="sidebar-sticky">
                     <ul class="nav flex-column">
                         <?php
                         require_once 'menu_temp.php';
                         ?>
+                        <!-- 其他菜單項目 -->
                     </ul>
                 </div>
             </nav>
 
             <!-- 主要內容 -->
-            <main role="main">
-                <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-                    <h1 class="page-title">後台訂單管理</h1>
-                </div>
-                <div class="card">
-                    <div class="card-body">
-                        <!-- 這裡可以放置訂單管理的內容 -->
-                        <p>歡迎使用後台訂單管理系統</p>
-                    </div>
-                </div>
+            <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
+               <h1>後台訂單管理</h1>
             </main>
         </div>
     </div>
