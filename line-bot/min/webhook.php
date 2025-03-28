697c96dbc782518ccedd032c913f0c4b65f499d4
SuHsienChin <akindpepe@gmail.com>
2025-03-29 03:40:36 +0800

    M

--- line-bot/min/webhook.php
+++ line-bot/min/webhook.php
@@ -17,136 +17,115 @@ function handleEvent($event) {
         $userId = $event['source']['userId'];
         $messageText = $event['message']['text'];
 
-        // 檢查用戶是否傳送 價目表
+        // 定義價目表數據
+        $priceList = [
+            ["name" => "毛孩形象全檔方案", "price" => "NT.5980"],
+            ["name" => "毛孩親寫真", "price" => "NT.600"],
+            ["name" => "毛孩與你親子寫真", "price" => "NT.1200"],
+            ["name" => "毛孩BOOM起來", "price" => "NT.800"]
+        ];
+
+        // 檢查用戶是否傳送「價目表」
         if (trim($messageText) == "價目表") {
-            $flexMessage = [
+            // 生成 Flex Message
+            $flexMessage = generateFlexMessage($priceList);
+
+            // 生成 Quick Reply
+            $quickReply = generateQuickReply($priceList);
+
+            // 發送回覆
+            replyMessage($event['replyToken'], [
                 "type" => "flex",
                 "altText" => "價目表",
-                "contents" => [
-                    "type" => "bubble",
-                    "body" => [
-                        "type" => "box",
-                        "layout" => "vertical",
-                        "contents" => [
-                            [
-                                "type" => "text",
-                                "text" => "寵物攝影服務價目表",
-                                "weight" => "bold",
-                                "size" => "xl",
-                                "color" => "#1DB446"
-                            ],
-                            [
-                                "type" => "separator",
-                                "margin" => "xxl"
-                            ],
-                            [
-                                "type" => "box",
-                                "layout" => "vertical",
-                                "margin" => "xxl",
-                                "spacing" => "sm",
-                                "contents" => [
-                                    [
-                                        "type" => "box",
-                                        "layout" => "horizontal",
-                                        "contents" => [
-                                            ["type" => "text", "text" => "毛孩形象全檔方案", "size" => "sm", "color" => "#555555"],
-                                            ["type" => "text", "text" => "NT.5980", "size" => "sm", "color" => "#111111", "align" => "end"]
-                                        ]
-                                    ],
-                                    [
-                                        "type" => "box",
-                                        "layout" => "horizontal",
-                                        "contents" => [
-                                            ["type" => "text", "text" => "毛孩親寫真", "size" => "sm", "color" => "#555555"],
-                                            ["type" => "text", "text" => "NT.600", "size" => "sm", "color" => "#111111", "align" => "end"]
-                                        ]
-                                    ],
-                                    [
-                                        "type" => "box",
-                                        "layout" => "horizontal",
-                                        "contents" => [
-                                            ["type" => "text", "text" => "毛孩與你親子寫真", "size" => "sm", "color" => "#555555"],
-                                            ["type" => "text", "text" => "NT.1200", "size" => "sm", "color" => "#111111", "align" => "end"]
-                                        ]
-                                    ],
-                                    [
-                                        "type" => "box",
-                                        "layout" => "horizontal",
-                                        "contents" => [
-                                            ["type" => "text", "text" => "毛孩BOOM起來", "size" => "sm", "color" => "#555555"],
-                                            ["type" => "text", "text" => "NT.800", "size" => "sm", "color" => "#111111", "align" => "end"]
-                                        ]
-                                    ]
-                                ]
-                            ]
-                        ]
-                    ]
-                ]
-            ];
-            
-            $body = [
-                "replyToken" => $event['replyToken'],
-                "messages" => [$flexMessage],
-                "quickReply" => [
-                    "items" => [
-                        [
-                            "type" => "action",
-                            "action" => [
-                                "type" => "message",
-                                "label" => "毛孩形象全檔",
-                                "text" => "我想了解毛孩形象全檔方案"
-                            ]
-                        ],
-                        [
-                            "type" => "action",
-                            "action" => [
-                                "type" => "message",
-                                "label" => "毛孩親寫真",
-                                "text" => "我想了解毛孩親寫真"
-                            ]
-                        ],
-                        [
-                            "type" => "action",
-                            "action" => [
-                                "type" => "message",
-                                "label" => "毛孩與你親子寫真",
-                                "text" => "我想了解毛孩與你親子寫真"
-                            ]
-                        ],
-                        [
-                            "type" => "action",
-                            "action" => [
-                                "type" => "message",
-                                "label" => "毛孩BOOM起來",
-                                "text" => "我想了解毛孩BOOM起來方案"
-                            ]
-                        ]
-                    ]
-                ]
-            ];
-            
-            replyMessage($event['replyToken'], $body, true);
+                "contents" => $flexMessage
+            ], $quickReply);
         } else {
-            // 如果不是 [價目表]，回覆其他提示
-            replyMessage($event['replyToken'], "請輸入 [價目表] 查看詳細資訊！");
+            // 如果不是「價目表」，回覆其他提示
+            replyMessage($event['replyToken'], "請輸入「價目表」查看詳細資訊！");
         }
     }
 }
 
+// 生成 Flex Message
+function generateFlexMessage($priceList) {
+    $contents = [];
+    foreach ($priceList as $item) {
+        $contents[] = [
+            "type" => "box",
+            "layout" => "horizontal",
+            "contents" => [
+                ["type" => "text", "text" => $item["name"], "size" => "sm", "color" => "#555555"],
+                ["type" => "text", "text" => $item["price"], "size" => "sm", "color" => "#111111", "align" => "end"]
+            ]
+        ];
+    }
+
+    return [
+        "type" => "bubble",
+        "body" => [
+            "type" => "box",
+            "layout" => "vertical",
+            "contents" => [
+                [
+                    "type" => "text",
+                    "text" => "寵物攝影服務價目表",
+                    "weight" => "bold",
+                    "size" => "xl",
+                    "color" => "#1DB446"
+                ],
+                [
+                    "type" => "separator",
+                    "margin" => "xxl"
+                ],
+                [
+                    "type" => "box",
+                    "layout" => "vertical",
+                    "margin" => "xxl",
+                    "spacing" => "sm",
+                    "contents" => $contents
+                ]
+            ]
+        ]
+    ];
+}
+
+// 生成 Quick Reply
+function generateQuickReply($priceList) {
+    $items = [];
+    foreach ($priceList as $item) {
+        $items[] = [
+            "type" => "action",
+            "action" => [
+                "type" => "message",
+                "label" => $item["name"],
+                "text" => "我想了解" . $item["name"]
+            ]
+        ];
+    }
+
+    return [
+        "items" => $items
+    ];
+}
+
 // 回覆用戶訊息
-function replyMessage($replyToken, $content, $isJson = false) {
+function replyMessage($replyToken, $message, $quickReply = null) {
     $url = "https://api.line.me/v2/bot/message/reply";
     $headers = [
         "Authorization: Bearer /tawKQINYfBLEp75MXH+HMsQ1Hw/IT1UZAnC0nxge0clIvgoBjBUE1Tr+LIhIhIpfa9TfYYgx1pTClW8z1UYK/iALlqXv6NDXe7G5PsemziQxAuDFOGpyHHqxP0b51gMjkz8Kmo0jCULhNm7A4P4VAdB04t89/1O/w1cDnyilFU=",
         "Content-Type: application/json"
     ];
-    
-    $body = $isJson ? $content : [
+
+    // 組合回覆內容
+    $body = [
         "replyToken" => $replyToken,
-        "messages" => [
-            ["type" => "text", "text" => $content]
-        ]
+        "messages" => [$message]
     ];
+
+    // 如果有 Quick Reply，加入到回覆中
+    if ($quickReply) {
+        $body["messages"][0]["quickReply"] = $quickReply;
+    }
 
     $options = [
         "http" => [
@@ -157,7 +136,13 @@ function replyMessage($replyToken, $content, $isJson = false) {
     ];
 
     $context = stream_context_create($options);
-    $response = file_get_contents($url, false, $context);
-    error_log("回覆結果：" . $response);
+    $response = @file_get_contents($url, false, $context);
+
+    // 錯誤處理
+    if ($response === FALSE) {
+        error_log("API 請求失敗：" . print_r($http_response_header, true));
+    } else {
+        error_log("回覆結果：" . $response);
+    }
 }
 ?>
