3b200e12c1092ee09a9d3376ee61d55d53059b82
SuHsienChin <akindpepe@gmail.com>
2025-03-29 13:59:21 +0800

    fix: 更新 webhook.php 以从 .env 文件读取 LINE 访问令牌
    为了增强安全性，将 LINE 访问令牌从代码中移除，改为从 .env 文件读取。同时添加了 .gitignore 文件以忽略 .env 文件，防止敏感信息泄露。

--- line-bot/min/webhook.php
+++ line-bot/min/webhook.php
@@ -1,4 +1,7 @@
 <?php
+// 載入 .env 檔案
+$env = parse_ini_file(__DIR__ . '/.env');
+
 // 取得 LINE 傳送的請求資料
 $input = file_get_contents('php://input');
 $data = json_decode($input, true);
@@ -160,9 +163,10 @@ function generateQuickReply($priceList) {
 
 // 回覆用戶訊息
 function replyMessage($replyToken, $message, $quickReply = null) {
+    global $env;
     $url = "https://api.line.me/v2/bot/message/reply";
     $headers = [
-        "Authorization: Bearer suczLIKWyfw0Ne6sBmW0UadXscpJ1zcHCkXvW73Fef+RQO6ojcOuV4G9nYeM+k1+Rj+AC7qEy9WuQ0FjwBvH819PXeQTmkzXCVu35xibdIy8HsH/KuE9LJFhZW+Lqdbny/EIRBsvx1SRNew8+OKJIAdB04t89/1O/w1cDnyilFU=",
+        "Authorization: Bearer " . $env['LINE_CHANNEL_ACCESS_TOKEN'],
         "Content-Type: application/json"
     ];
 
