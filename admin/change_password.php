7904966a7978e60606418a611dde8eae2f322671
SuHsienChin <akindpepe@gmail.com>
2025-04-11 21:01:26 +0800

    M

--- admin/change_password.php
+++ admin/change_password.php
@@ -1,9 +1,5 @@
 <?php
-session_start();
-if (!isset($_SESSION['admin_id'])) {
-    header('Location: index.php');
-    exit;
-}
+
 
 require_once '../databaseConnection.php';
 $dbConnection = new DatabaseConnection();
