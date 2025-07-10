fd476439c5272efdbc9ccb7af12b05cf13530bff
SuHsienChin <akindpepe@gmail.com>
2025-07-11 04:14:31 +0800

    fix: 修正自動載入路徑並啟用錯誤顯示
    修正 vendor 自動載入檔案的路徑錯誤
    新增錯誤顯示設定以方便開發除錯

--- line-bot/php-line-bot/bot.php
+++ line-bot/php-line-bot/bot.php
@@ -1,6 +1,9 @@
 <?php
 // 引入 Composer 自動載入的檔案
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '../vendor/autoload.php';
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 
 // 引入 SDK 的必要類別
 use LINE\LINEBot;
